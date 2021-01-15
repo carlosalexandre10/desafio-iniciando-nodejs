cffed4b1a923e6efaba4353cdb148e8fa9d97198
Carlos Alexandre <carlos11alexandre11@gmail.com>
2021-01-15 13:33:24 -0300

    Finalizado

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -52,7 +52,7 @@ transactionsRouter.post(
   async (request, response) => {
     const importTransactions = new ImportTransactionsService();
 
-    const transactions = await importTransactions.execute();
+    const transactions = await importTransactions.execute(request.file.path);
 
     return response.json(transactions);
   },
